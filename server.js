770e1edbf274fd88f00dc3521e39aea9b1ec3ac2
Basu Khanal <87610840+bkhanal87@users.noreply.github.com>
2021-12-09 13:56:32 -0500

    commented usetopology and create index at server.js

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/cryptic-shore-46384",
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
+    // useUnifiedTopology: true,
+    // useCreateIndex: true,
     useFindAndModify: false,
   }
 );
