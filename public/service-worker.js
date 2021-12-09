7bb1c8126cebc706286108f33aa2cf46617b9407
Basu Khanal <87610840+bkhanal87@users.noreply.github.com>
2021-12-09 11:41:34 -0500

    added nodemon dev d

--- public/service-worker.js
+++ public/service-worker.js
@@ -15,7 +15,8 @@ const FILES_TO_CACHE = [
 // install event handler
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("static").then((cache) => {
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Files cached successfully!");
       return cache.addAll(FILES_TO_CACHE);
     })
   );
@@ -23,8 +24,51 @@ self.addEventListener("install", (event) => {
   self.skipWaiting();
 });
 
-// retrieve assets from cache
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener("activate", (event) => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event
+    .waitUntil(
+      caches.keys().then((keyList) => {
+        return Promise.all(
+          keyList.map((key) => {
+            if (key !== CACHE_NAME && key != DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    )
+    .then(() => self.clients.claim());
+});
+
+// fetch
+
 self.addEventListener("fetch", (event) => {
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(event.request)
+            .then((response) => {
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+              return response;
+            })
+            .catch((error) => {
+              return cache.match(event.request);
+            });
+        })
+        .catch((error) => console.log(error))
+    );
+    return;
+  }
+
+  // retrieve assets from cache
+
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
